8afa9399658702fbf6bb4539a0105551945b5a98
leehyeonj <billaa@naver.com>
2021-06-27 19:24:05 +0900

    버킷리스트 추가하기 버튼

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -22,6 +22,18 @@ class App extends React.Component {
     console.log(this.text);
 		console.log(this.text.current);
   }
+  addBucketList = () => {
+    let list = this.state.list;
+    const new_item = this.text.current.value;
+
+    // 리액트에서는 concat으로 배열항목을 합쳐주는 게 좋아요. 아래처럼요!
+    // list = list.concat(new_item);
+    // this.setState({list: list});
+
+    //  이건 가장 편한 배열 합치기 방법입니다. 
+    //  ...은 배열 안에 있는 항목을 전부 꺼내서 늘어놓는다는 뜻입니다. (스프레드 문법이라고 불러요.)
+    this.setState({list: [...list, new_item]});
+  }
 
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
@@ -36,14 +48,23 @@ class App extends React.Component {
           <BucketList list={this.state.list} />
         </Container>
 
-        <div>
+        <Input>
           <input type="text" ref={this.text}/>
-        </div>
+          <button onClick={this.addBucketList}>추가하기</button>
+        </Input>
       </div>
     );
   }
 }
-
+const Input = styled.div`
+  max-width: 350px;
+  min-height: 10vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
 const Container = styled.div`
   max-width: 350px;
   min-height: 80vh;
