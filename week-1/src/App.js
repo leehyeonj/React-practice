307ac263e9a3ba017549cee3520fb74c31a1d229
leehyeonj <billaa@naver.com>
2021-06-27 18:14:08 +0900

    React 요소를 가지고 오는 방법 React.createRef()

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,59 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
 // BucketList 컴포넌트를 import 해옵니다.
 // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
-import BucketList from './BucketList';
-import "./scss_ex.scss";
-// import './style.css';
+import BucketList from "./BucketList";
 import styled from "styled-components";
 
 // 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     // App 컴포넌트의 state를 정의해줍니다.
     this.state = {
-      list: ['영화관 가기', '매일 책읽기', '수영 배우기'],
+      list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
     };
+    // ref는 이렇게 선언합니다! 
+    this.text = React.createRef();
+  }
+
+  componentDidMount(){
+		// 콘솔에서 확인해보자!
+    console.log(this.text);
+		console.log(this.text.current);
   }
 
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
-    // this 키워드를 통해 state에 접근할 수 있어요.
-    console.log(this.state);
-
-      return (
+    
+    return (
       <div className="App">
         <Container>
-            <Title>내 버킷리스트</Title>
-            <Line/>
-            {/* 컴포넌트를 넣어줍니다. */}
-            {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
-            <BucketList list={this.state.list} />
+          <Title>내 버킷리스트</Title>
+          <Line />
+          {/* 컴포넌트를 넣어줍니다. */}
+          {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
+          <BucketList list={this.state.list} />
         </Container>
+
+        <div>
+          <input type="text" ref={this.text}/>
+        </div>
       </div>
     );
   }
 }
 
 const Container = styled.div`
-    max-width: 350px;
-    min-height: 80vh;
-    background-color: #fff;
-    padding: 16px;
-    margin: 20px auto;
-    border-radius: 5px;
-    border: 1px solid #ddd;
+  max-width: 350px;
+  min-height: 80vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
 `;
 
 const Title = styled.h1`
-    color: slateblue;
-    text-align: center;
+  color: slateblue;
+  text-align: center;
 `;
 
 const Line = styled.hr`
-    margin: 16px 0px;
-    border: 1px dotted #ddd;
+  margin: 16px 0px;
+  border: 1px dotted #ddd;
 `;
+
 export default App;
