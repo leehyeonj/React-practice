24da76bb5ce34f485f6fae845ca0f346a0a8c644
leehyeonj <billaa@naver.com>
2021-06-27 15:31:13 +0900

    클래스형 컴포넌트

--- week-1/src/BucketList.js
+++ week-1/src/BucketList.js
@@ -1,22 +1,20 @@
 // 리액트 패키지를 불러온다.
 import React from 'react'; 
 
-// 함수형 컴포넌트는 이렇게 쓸 수도 있고
-// function Bucketlist(props){
-//     return (
-//         <div>버킷 리스트</div>
-//     );
-// }
-
-// 이렇게 쓸 수도 있다.. =>가 들어간 함수를 화살표 함수라고 한다.
-// ()안에 props는 부모 컴포넌트부터 받아온 데이터다.
-// 얘의 부모가 누구냐?
+// App.js에서 받아온 props
 const BucketList = (props) => {
 
+    const my_lists = props.list;
+
+    console.log(props);
     // 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트)를 반환한다.
     return (
         <div>
-            버킷 리스트
+            {
+                my_lists.map((list, index)=>{
+                    return(<div key ={index}>{list}</div>);
+                })
+             }
         </div>
     );
 }
