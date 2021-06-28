7e77ea4f1398ab7ac74ead785599b31f28b014bc
leehyeonj <billaa@naver.com>
2021-06-28 15:07:14 +0900

    우정테스트 랭킹 페이지 결과페이지

--- friend-test/src/App.js
+++ friend-test/src/App.js
@@ -1,54 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import {Route, Switch} from "react-router-dom";
 
 import Start from "./Start";
 import Quiz from "./Quiz";
 import Score from "./Score";
+import Message from "./Message";
+import Ranking from "./Ranking";
+
+import { withRouter } from "react-router";
+// 리덕스 스토어와 연결하기 위해 connect라는 친구를 호출할게요!
+import { connect } from "react-redux";
+
+
+// 이 함수는 스토어가 가진 상태값을 props로 받아오기 위한 함수예요.
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+// 이 함수는 값을 변화시키기 위한 액션 생성 함수를 props로 받아오기 위한 함수예요.
+const mapDispatchToProps = (dispatch) => ({
+  load: () => {
+    
+  },
+  
+});
 
 class App extends React.Component{
   constructor(props){
     super(props);
-// state에 필요한 데이터를 넣어줘요!
+
     this.state = {
-      name: "꼬미와 친구들",
-      page: "quiz",
-      scoreMsg: "이 정도면 아주 친한 친구 사이! 앞으로도 더 친하게 지내요! :)",
-      list: [
-        { question: "꼬미는 2살이다.", answer: "X" },
-        { question: "꼬미는 남자다.", answer: "O" },
-        { question: "꼬미는 딸기를 좋아한다.", answer: "O" },
-        { question: "꼬미는 눈물이 많다.", answer: "O" },
-        { question: "꼬미는 혼자 있을 때 잔다.", answer: "O" },
-        { question: "꼬미는 수박을 좋아한다.", answer: "O" },
-        { question: "꼬미는 엄마를 제일 좋아한다.", answer: "O" },
-        { question: "꼬미는 3KG이다.", answer: "X" },
-        { question: "꼬미는 귀엽다.", answer: "O" },
-        { question: "꼬미는 풀을 좋아한다.", answer: "O" },
-        { question: "꼬미는 청소기를 무서워한다.", answer: "O" },
-      ],
-      ranking: [
-        { rank: 1, name: "이현주", message: "꼬미♥" },
-        { rank: 1, name: "이현주", message: "꼬미♥" },
-        { rank: 1, name: "이현주", message: "꼬미♥" },
-        { rank: 1, name: "이현주", message: "꼬미♥" },
-        { rank: 1, name: "이현주", message: "꼬미♥" },
-        { rank: 1, name: "이현주", message: "꼬미♥" },
-        { rank: 1, name: "이현주", message: "꼬미♥" },
-      ],
+      
     };
   }
 
   render () {
     return (
       <div className="App">
-        {/* 조건부 랜더링을 합니다 / state의 page를 바꿔가면서 확인해봐요! */}
-        {this.state.page === "quiz" && (<Quiz list={this.state.list} />)}
-        {this.state.page === "start" && (<Start name={this.state.name} />)}
-        {this.state.page === "score" && (<Score name={this.state.name} scoreMsg={this.state.scoreMsg}/>)}
+        <Switch>
+          <Route path="/quiz" component={Quiz} />
+          <Route path="/" exact component={Start} />
+          <Route path="/score" component={Score} />
+          <Route path="/message" component={Message} />
+          <Route path="/ranking" component={Ranking} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
--- friend-test/src/Quiz.js
+++ friend-test/src/Quiz.js
@@ -1,22 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-import img from "./scc_img.jpg";
-import TinderCard from "react-tinder-card";
+import Score from "./Score";
 import SwipeItem from "./SwipeItem";
+import TinderCard from "react-tinder-card";
 
-//
+
+import { useSelector, useDispatch } from "react-redux";
+import {addAnswer} from "./redux/modules/quiz";
+
 const Quiz = (props) => {
-  console.log(props);
-  // state로 관리하자!
-  const [num, setNum] = React.useState(0);
+  const dispatch = useDispatch();
+  const answers = useSelector((state) => state.quiz.answers);
+  const quiz = useSelector((state) => state.quiz.quiz);
+  
+  const num = answers.length;
 
   const onSwipe = (direction) => {
-    console.log("You swiped: " + direction);
-    setNum(num + 1);
-  };
+    let _answer = direction === "left"? "O" : "X";
 
-  if (num > 10) {
-    return <div>퀴즈 끝!</div>;
+    if(_answer === quiz[num].answer){
+      // 정답일 경우,
+      dispatch(addAnswer(true));
+    }else{
+      // 오답일 경우,
+      dispatch(addAnswer(false));
+    }
+  }
+
+  if (num > quiz.length -1) {
+    return <Score {...props}/>;
+    // return <div>퀴즈 끝!</div>;
   }
 
   return (
@@ -24,7 +37,7 @@ const Quiz = (props) => {
       <p>
         <span>{num + 1}번 문제</span>
       </p>
-      {props.list.map((l, idx) => {
+      {quiz.map((l, idx) => {
         if (num === idx) {
           return <Question key={idx}>{l.question}</Question>;
         }
@@ -35,21 +48,9 @@ const Quiz = (props) => {
         <Answer>{" X"}</Answer>
       </AnswerZone>
 
-      {props.list.map((l, idx) => {
+      {quiz.map((l, idx) => {
         if (idx === num) {
-          return <SwipeItem key={idx} onSwipe={onSwipe} />;
-          // return (
-          //   <DragItem key={idx}>
-          //     <TinderCard
-          //       onSwipe={onSwipe}
-          //       onCardLeftScreen={onSwipe}
-          //       onCardRightScreen={onSwipe}
-          //       preventSwipe={["up", "down"]}
-          //     >
-          //       <img src={img} />
-          //     </TinderCard>
-          //   </DragItem>
-          // );
+          return <SwipeItem key={idx} onSwipe={onSwipe}/>;
         }
       })}
     </QuizContainer>
@@ -57,6 +58,8 @@ const Quiz = (props) => {
 };
 
 const QuizContainer = styled.div`
+  margin-top: 16px;
+  width: 100%;
   & > p > span {
     padding: 8px 16px;
     background-color: #fef5d4;
